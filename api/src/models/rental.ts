a1c9f8150abacd2cc1768c437498a3fdc24a6e14
Fuzail Ahmed <147535109+Fuzailahmed04@users.noreply.github.com>
2024-12-18 22:21:59 +0500

    migration and table for rental_details

---
+++ api/src/models/rental.ts
@@ -0,0 +1,89 @@
+import { Model, DataTypes, Sequelize } from "sequelize";
+import { v4 as uuidv4 } from 'uuid';
+import sequelizeInit from '../config/sequelize';
+
+class RentalDetails extends Model {
+  public rental_id!: string;
+  public property!: string;
+  public street_address!: string;
+  public appartment!: string;
+  public rent_amount!: string;
+  public due_date!: Date;
+  public month_to_month!: boolean;
+  public lease_start_date!: Date;
+  public lease_end_date!: Date;
+  public lease_type!: string;
+  public lease_co_signers!: string;
+  public lease_source!: string;
+
+
+  static associate(models: any) {
+  }
+}
+
+export default (sequelize: Sequelize): typeof RentalDetails => {
+  RentalDetails.init(
+    {
+      rental_id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: uuidv4,
+        allowNull: false,
+      },
+      property: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      street_address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      appartment: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      rent_amount: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      due_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      month_to_month: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      lease_start_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      lease_end_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      lease_type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lease_co_signers: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lease_source: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+     
+    },
+    {
+      sequelize:sequelizeInit,
+      modelName: "RentalDetails",
+      tableName: "rental_details",
+      timestamps: true,
+   
+    }
+  );
+
+  return RentalDetails;
+};
